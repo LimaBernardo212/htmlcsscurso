01f138e703c4514e4bc7cd597f6db18703b1f2d0
LimaBernardo212 <mariahelenalima911@gmail.com>
2024-01-09 15:02:14 -0300

    Atualização dia 09/01/24

--- EXERCICIOS/Ex027/cad.php
+++ EXERCICIOS/Ex027/cad.php
@@ -1,17 +1,105 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    <p>
-    <?php
-        $nome = $_GET["t1"] ?? "<Desconhecido>";
-        $snome = $_GET["t2"] ?? "Provavelmente drogado";
-        echo "Seja bem vindo $nome $snome";
-        ?>
-    </p>
-</body>
-</html>
+Uma piada pra um trouxa:
+Qual é o desastre natural que os cães mais odeiam?
+Fura-cão;
+Qual é a peça de carro que é feita só no Egito?
+Os faraóis;
+O que o cadarço falou para o tênis?
+“Estou amarradão em você”;
+O engenheiro olhou para o espelho. O que aconteceu?
+O engenheiro civil;
+Na briga entre o esparadrapo e a fita isolante, quem ganhou?
+A fita isolante, pois ela é faixa preta;
+O que o menino fez para não ir ao oculista?
+Se mudou para Boa Vista;
+Quem é a mãe do mingau?
+A mãe zena;
+Qual é a roupa preferida do macaco?
+O macacão;
+Qual é a parte mais velha do carro?
+O vô-lante;
+Qual é a nota musical preferida dos carros?
+A ré
+Tenho uma enxada, uma pá e uma foice. Quantas ferramentas eu tenho?
+Duas, porque uma foi-se;
+Qual é o estado brasileiro que ama ferramentas?
+O Ama-pá;
+Por que a loja do canivete faliu?
+Porque só vendia a-fiado;
+Por que não falta energia no quartel?
+Porque todo cabo já foi soldado;
+Por que as rodas do trem são de ferro?
+Porque se fossem de borracha apagariam a linha;
+Nunca desista de seus sonhos
+Se acabou em uma padaria, busque em outra;
+Por que o jacaré tirou o jacarezinho da escola?
+Porque ele réptil de ano;
+Quem fala errado: a Mônica ou o Cebolinha?
+A Mônica, pois o Cebolinha fala “elado”;
+O que uma impressora falou para outra?
+“Esse papel é seu ou é impressão minha?”
+O que a lâmpada falou quando a ligaram?
+“Tô ligada”;
+O que o lápis disse para o apontador?
+“Estou desapontado”;
+Que animal já passou da validade?
+O javali;
+Por que a criança comeu a lição de matemática?
+Porque a professora disse que fazer a lição era mamão com açúcar;
+O que é um pontinho rosa no estádio de luta dos Pokémon?
+É o invencível PINKachu;
+Por que a água foi presa?
+Porque matou a sede;
+Qual bicho que não é caro?
+A barata;
+Como se chama o primo vegetariano do Bruce Lee?
+É o Bróco Lee;
+Qual é o esporte preferido dos músicos?
+Lançamento de disco;
+O que o azeite disse para o vinagre?
+“Falo nada, só óleo”;
+O que a máquina de calcular disse para o contador?
+“Pode contar comigo!”;
+O que a vaca foi fazer no espaço?
+Procurar o vácuo;
+Qual é o problema do decorador quando foi procurar um médico?
+De-Coração;
+Qual é o som que os porcos-espinhos fazem quando se beijam?
+“Ui”;
+Sabe o que o Batman falou para o Homem Invisível?
+“Há quanto tempo não te vejo!”;
+Qual é o país que mais produz vinho?
+Uva-ticano;
+Como acaba um jogo de futebol entre patos?
+Empatado;
+Qual é o nome do carro que anuncia que vai chover?
+Celta preto;
+Em uma corrida de frutas, a maçã está ganhando. O que acontece se ela diminuir a velocidade?
+A uva passa;
+Qual é a comida mais devagar?
+A po-lente;
+Como a cobra salva uma pessoa em uma enchente?
+Dando o bote;
+Qual é o prato preferido do Thor?
+Thorresmo;
+O que o poste falou para o cachorro?
+“Não adianta regar que eu não cresço mais”;
+Qual é o rio mais azedo do mundo?
+O Rio Solimões;
+O que o cavalo foi fazer no orelhão?
+Passar trote;
+Duas pessoas estão em uma canoa no lago. Quando o remo cai na lagoa, as pessoas começam a procurá-lo. Qual é o nome do filme?
+Procurando Remo;
+Qual é a diferença entre uma lagoa e uma padaria?
+Na lagoa há sapinho e na padaria, assa pão;
+Quem é mais velho: a lua ou o sol?
+A lua, porque pode sair à noite;
+O que o porco-espinho perguntou para o cacto?
+“É você, mamãe?”;
+Qual é o vinho que não tem álcool?
+Ovinho de codorna;
+Qual é o lugar mais certo do Brasil?
+O sertão.
+Qual é a ordem das piadas do Google Assistente?
+As piadas do Google Assistente são contadas de forma aleatória com base em uma biblioteca pré-programada. Esse conteúdo é atualizado regularmente com novas inserções e pode ser personalizado conforme interações com os usuários.
+
+Além disso, o ajudante digital também consegue contar diferentes estilos de piadas, o que inclui trocadilhos, perguntas e respostas, piadas com temática de animais ou tecnologia, entre outros.
